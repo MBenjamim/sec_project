69e902ff9eeb51744aec128293648f5c41713921
joaoafonso07 <joaoafonsomestre@gmail.com>
2025-03-08 22:18:43 +0000

    scripting: add multiscreen tmux script and add excution permission to all sh scripts

---
+++ run_multiscreen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source load_config_and_compile.sh
+
+# Start the first server in a new tmux session
+tmux new-session -d -s blockchain_network -n server_0 "mvn exec:java -Dexec.mainClass=main.java.server.BlockchainNetworkServer -Dexec.args=\"0 $BASE_PORT_SERVER_TO_SERVER $BASE_PORT_CLIENT_TO_SERVER\""
+if [ $? -ne 0 ]; then
+    echo "Failed to create tmux session."
+    exit 1
+fi
+
+# Start additional servers in separate tmux panes
+for ((i=1; i<$NUM_SERVERS; i++)); do
+    SERVER_PORT=$((BASE_PORT_SERVER_TO_SERVER + i))
+    CLIENT_PORT=$((BASE_PORT_CLIENT_TO_SERVER + i))
+    if [ $((i % 2)) -eq 1 ]; then
+        tmux split-window -h "mvn exec:java -Dexec.mainClass=main.java.server.BlockchainNetworkServer -Dexec.args=\"$i $SERVER_PORT $CLIENT_PORT\""
+    else
+        tmux split-window -v "mvn exec:java -Dexec.mainClass=main.java.server.BlockchainNetworkServer -Dexec.args=\"$i $SERVER_PORT $CLIENT_PORT\""
+    fi
+    tmux select-layout tiled
+    sleep 1
+done
+
+# Attach to tmux session
+tmux attach-session -t blockchain_network
